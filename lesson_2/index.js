21c68cf789132b3c98b7c4a15a452d4901341c59
marenkol <marenkolilia@gmail.com>
2019-05-12 18:38:17 +0300

    lessonn 2

---
+++ lesson_2/index.js
@@ -0,0 +1,15 @@
+// Task 1
+function makeGreetings ( age ) {
+	let ageWord = 'год'
+
+	if ( age % 10 >= 2 && age % 10 < 5 ) {
+		ageWord = 'года'
+	}
+
+	if ( ( age % 10 >= 5 && age % 10 < 10 ) || ( age > 10 && age <= 19 ) || age % 10 == 0 ) {
+		ageWord = 'лет'
+	}
+
+	return 'Мой возраст ' + age + ' ' + ageWord
+}
+
