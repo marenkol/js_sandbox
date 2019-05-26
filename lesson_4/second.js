27256b0c62aa0620e4f93b4335361b68feea003c
marenkol <marenkolilia@gmail.com>
2019-05-26 23:28:20 +0300

    Task 3 v1. 4_2

--- lesson_4/second.js
+++ lesson_4/second.js
@@ -1,20 +1,64 @@
-/*Part 2: print, в файле second.js
-Создать функцию print, которая принимает один аргумент - размер фигуры, возвращает строку, в которой символы составляют “стрелку” нужного размера, работу иллюстрируют примеры (символ ` обозначает начало и конец строки, в задаче необходимо использовать символ переноса строки):*/
-
+/*Part 2: */
 
 function print(size){
 	let string = '';
+	// let spaces = 0;
 
 	string = string + '\n';
 	for ( let i = 0; i < size;  i++){
-		string = string + '*';
-		for ( let j = i; j < size-1; j++){
+		for ( let spaces = 0 ; spaces <= i ; spaces++){
 			string = string + ' ';
 		}
+
+		string = string + '*';
+		for ( let j = i; j < size-1; j++){
+			string = string + '  ';
+		}
+
 		string = string + '*\n';
 	}
 
 	return string;
 }
 
-print(5);
+
+
+
+/* Part 3 */
+function calculate() {
+	let result = {
+		history: [],
+		formula: '',
+		args: {}
+	}
+
+	let y = 0;
+	let formulaSelected = prompt('Choose some formula. 1 - y = kx + b,  2 - y = x^2, exit', 1);
+
+	if ( formulaSelected == 1 ){
+		let k = prompt("k = ", 1);
+		let x = prompt("x = ", 1);
+		let b = prompt("b = ", 1);
+
+		y = Number(k) * Number(x) + Number(b);
+		result.history.push(k, x, b);
+		console.log(result.history);
+		result.formula = 'y = kx + b';
+		result.args.k = k;
+		result.args.x = x;
+		result.args.b = b;
+	} else if ( formulaSelected == 2){
+		let x = prompt("x = ", 1);
+
+		y = Number(x) * Number(x);
+		result.formula = 'y = x^2';
+		result.history.push(x);
+		result.args = {};
+		result.args[x] = x
+	} else {
+		calculate()
+	}
+
+
+	return result;
+}
