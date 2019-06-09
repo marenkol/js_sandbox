71943206adc48178c69cd774358bc81f1d0232c0
marenkol <marenkolilia@gmail.com>
2019-06-09 16:22:44 +0300

    Task 2. Lesson 7. V2

--- lesson_7/index.js
+++ lesson_7/index.js
@@ -1,25 +1,29 @@
 class MiniSlider {
 	constructor(sliderID){
 		this.miniSlider = document.querySelector(`#${sliderID}`);
+		this.elements = this.miniSlider.querySelectorAll('.mini-slider__imagewrap');
 
-		// console.log(this.miniSlider)
+		this.activeSlideIndex = 0;
 		this.hideAll();
-		this.show(1);
+		this.show(this.activeSlideIndex);
 		this.createButtons();
+
+		this.nextBtn = this.miniSlider.querySelector('.mini-slider__next');
+		this.nextBtn.addEventListener('click', this.next.bind(this));
+
+		this.prevBtn = this.miniSlider.querySelector('.mini-slider__prev');
+		this.prevBtn.addEventListener('click', this.prev.bind(this));
 	}
 
 	hideAll(){
-		const elements = this.miniSlider.querySelectorAll('img');
-		// console.log(elements);
-
-		for (let i = 0; i < elements.length; i += 1) {
-			elements[i].classList.add('hidden');
+		for (let i = 0; i < this.elements.length; i += 1) {
+			this.elements[i].classList.add('hidden');
 		}
 	}
 
 	show(n){
-		const elements = this.miniSlider.querySelectorAll('img');
-		elements[n].classList.remove('hidden');
+		this.elements[n].classList.remove('hidden');
+		this.elements[n].classList.add('active');
 	}
 
 	createButtons(){
@@ -28,18 +32,41 @@ class MiniSlider {
 
 		next.classList.add('mini-slider__next');
 		prev.classList.add('mini-slider__prev');
-		console.log(next);
 
 		prev.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M15.41 16.59L10.83 12l4.58-4.59L14 6l-6 6 6 6 1.41-1.41z"/><path fill="none" d="M0 0h24v24H0V0z"/></svg>';
 		next.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M8.59 16.59L13.17 12 8.59 7.41 10 6l6 6-6 6-1.41-1.41z"/><path fill="none" d="M0 0h24v24H0V0z"/></svg>';
 
 		this.miniSlider.appendChild(next);
 		this.miniSlider.appendChild(prev);
-	}
+	};
+
 
 	next(){
+		this.activeSlideIndex += 1;
 
+		if( this.activeSlideIndex >= this.elements.length ){
+			this.activeSlideIndex = 0;
+		}
+
+		this.hideAll();
+		this.show(this.activeSlideIndex);
+		// console.log(this.activeSlideIndex);
+	}
+
+	prev(){
+		this.activeSlideIndex -= 1;
+
+		if( this.activeSlideIndex < 0 ){
+			this.activeSlideIndex = this.elements.length - 1;
+		}
+
+		this.hideAll();
+		this.show(this.activeSlideIndex);
+		// console.log(this.activeSlideIndex);
 	}
 }
 
 window.MiniSlider  = MiniSlider;
+
+
+
