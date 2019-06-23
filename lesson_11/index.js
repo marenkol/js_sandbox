103a01f2d1ec758f81ec4dabe46be0c67506b63f
marenkol <marenkolilia@gmail.com>
2019-06-24 00:11:07 +0300

    Task 2. Lesson 11. V2

--- lesson_11/index.js
+++ lesson_11/index.js
@@ -23,45 +23,70 @@ function getIpData(url) {
 }
 
 
-// Promise.race([
-// 	getIpData('https://api6.ipify.org/?format=json'),
-// 	getIpData('https://api.2ip.ua/geo.json?ip='),
-// 	getIpData('http://api.sypexgeo.net/'),
-// ]).then ( IP => {
-// 	return fetch(`https://api.jsonbin.io/g/${IP}`, {
-// 				method: 'GET'
-// 			})
-// 			.then( response => response.json())
-// 			.then( obj => {
-// 				resultBox.innerHTML += `City: ${obj.data.city}, Country: ${obj.data.country}`;
-// 			} )
-// })
+Promise.race([
+	getIpData('https://api6.ipify.org/?format=json'),
+	getIpData('http://api.sypexgeo.net/'),
+	]).then ( IP => {
+		return fetch(`https://api.jsonbin.io/g/${IP}`, {
+			method: 'GET'
+		})
+		.then( response => response.json())
+		.then( obj => {
+			resultBox.innerHTML += `City: ${obj.data.city}, Country: ${obj.data.country}`;
+		} )
+	})
 
 
 
-/****************************************************************
-	Task 2
-****************************************************************/
+/* ***************************************************************
+Task 2
+*************************************************************** */
+
 const todoInput = document.querySelector('#todo-input');
 const buttonAdd = document.querySelector('#btn-add');
-const list = document.querySelector('#todo-list')
+const listTitle = document.querySelector('#list-title');
+const list = document.querySelector('#todo-list');
+let inputText = null;
+let listNum = 0;
+let listArr = [];
+const removeSVG = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0V0z"/><path d="M16 9v10H8V9h8m-1.5-6h-5l-1 1H5v2h14V4h-3.5l-1-1zM18 7H6v12c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7z"/></svg>'
 
 
-let inputText = '';
 const getInputText = event => {
 	inputText = event.target.value;
-	console.log(inputText)
-
+	// console.log(inputText)
 }
 todoInput.addEventListener('change', getInputText);
 
 
-const addItem = text => {
-	let newLi = document.createElement('li');
-	newLi.classList.add('list-group-item');
-  	newLi.innerHTML = inputText;
-	list.appendChild(newLi);
-	todoInput.value = "";
+const addItem = () => {
+	if ( inputText ) {
+		listArr.push({
+			id: `li-${++listNum}`,
+			text: inputText,
+			el: 'li',
+			class: `list-group-item`,
+			innerHTML: `<div class="d-flex justify-content-between"><span>${listNum}. ${inputText}</span><span id="remove-${listNum}" class="remove-icon">${removeSVG}</span></div>`
+		});
+
+		list.innerHTML = '';
+
+		listArr.forEach( element  => {
+			let newLi = document.createElement(element.el);
+			newLi.setAttribute('id', element.id);
+			newLi.classList.add(element.class);
+			newLi.innerHTML = element.innerHTML
+
+			list.appendChild(newLi);
+		})
+
+		if ( listArr.length ){
+			listTitle.classList.remove('d-none');
+		}
+
+		todoInput.value = '';
+		inputText = null;
+	}
 }
 buttonAdd.addEventListener('click', addItem);
 
