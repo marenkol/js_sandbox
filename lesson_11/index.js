84323d186696d4a5ef56961e441baa59a1a510e9
marenkol <marenkolilia@gmail.com>
2019-06-23 16:31:03 +0300

    Task 2. Lesson 11. V1

--- lesson_11/index.js
+++ lesson_11/index.js
@@ -23,18 +23,47 @@ function getIpData(url) {
 }
 
 
-Promise.race([
-	getIpData('https://api6.ipify.org/?format=json'),
-	getIpData('https://api.2ip.ua/geo.json?ip='),
-	getIpData('http://api.sypexgeo.net/'),
-]).then ( IP => {
-	return fetch(`https://api.jsonbin.io/g/${IP}`, {
-				method: 'GET'
-			})
-			.then( response => response.json())
-			.then( obj => {
-				resultBox.innerHTML += `City: ${obj.data.city}, Country: ${obj.data.country}`;
-			} )
-} )
+// Promise.race([
+// 	getIpData('https://api6.ipify.org/?format=json'),
+// 	getIpData('https://api.2ip.ua/geo.json?ip='),
+// 	getIpData('http://api.sypexgeo.net/'),
+// ]).then ( IP => {
+// 	return fetch(`https://api.jsonbin.io/g/${IP}`, {
+// 				method: 'GET'
+// 			})
+// 			.then( response => response.json())
+// 			.then( obj => {
+// 				resultBox.innerHTML += `City: ${obj.data.city}, Country: ${obj.data.country}`;
+// 			} )
+// })
+
+
+
+/****************************************************************
+	Task 2
+****************************************************************/
+const todoInput = document.querySelector('#todo-input');
+const buttonAdd = document.querySelector('#btn-add');
+const list = document.querySelector('#todo-list')
+
+
+let inputText = '';
+const getInputText = event => {
+	inputText = event.target.value;
+	console.log(inputText)
+
+}
+todoInput.addEventListener('change', getInputText);
+
+
+const addItem = text => {
+	let newLi = document.createElement('li');
+	newLi.classList.add('list-group-item');
+  	newLi.innerHTML = inputText;
+	list.appendChild(newLi);
+	todoInput.value = "";
+}
+buttonAdd.addEventListener('click', addItem);
+
 
 
